51c86d4ed4739615a15819a9e94daebb790c4629
Rohit Waldekar <Rohitwaldekar@gmail.com>
2022-11-06 19:57:56 +0530

    Added yahoo finaance and alphaca api code

--- src/__init__.py
+++ src/__init__.py
@@ -1,46 +1,18 @@
-# import requests
-# from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-# import json
-# import pandas as pd
-
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# api_key = os.getenv('APCA_API_KEY_ID')
-# api_secret = os.getenv('APCA_API_SECRET_KEY')
-
-# url = 'https://paper-api.alpaca.markets'
-# api_call = '/v2/assets/:APPL'
-
-# headers = {'content-type': 'application/json', 
-#            'Apca-Api-Key-Id': api_key, 
-#            'Apca-Api-Secret-Key': api_secret}
-
-# response = requests.get(url + api_call, headers=headers)
-# response = json.loads(response.text)
-# print(response)
-# assets = [dict(item) for item in response]
-# df = pd.DataFrame.from_records(assets)
-# print(df.head())
-###################################################################################################
 import os
 import sys
 from dotenv import load_dotenv
 import pandas as pd
-
-load_dotenv()
-
-api_key = os.getenv('APCA_API_KEY_ID')
-api_secret = os.getenv('APCA_API_SECRET_KEY')
+import pathlib
 
 from alpaca.data import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
 from alpaca.data.requests import StockLatestQuoteRequest
 from alpaca.data.requests import StockLatestBarRequest
 from alpaca.data.timeframe import TimeFrame
 # from alpaca.data import StockDataStream
 # from alpaca.data.live import StockDataStream
+
+import yfinance as yf
 
 '''
 # async handler
@@ -53,37 +25,50 @@ stock_stream.subscribe_quotes(quote_data_handler, "SPY")
 stock_stream.run()
 '''
 
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import StockBarsRequest
+load_dotenv()
+
+api_key = os.getenv('APCA_API_KEY_ID')
+api_secret = os.getenv('APCA_API_SECRET_KEY')
 
 # no keys required for crypto data
 client = StockHistoricalDataClient(api_key, api_secret)
 
 request_params = StockBarsRequest(
-                        symbol_or_symbols=["SPY", "GLD", "TLT"],
+                        symbol_or_symbols=["AAPL"],                #["SPY", "GLD", "TLT"],
                         timeframe=TimeFrame.Hour,
-                        start="2022-11-01 00:00:00",
-                        end="2022-11-02 00:00:00"
+                        start="2022-08-01 00:00:00",
+                        end="2022-11-01 00:00:00"
                  )
 
 bars = client.get_stock_bars(request_params)
-
+# print("1.",bars)
 # convert to dataframe
 # bars.df
-
+# d = bars["AAPL"]
+# print("1.",d.df)
 # access bars as list - important to note that you must access by symbol key
 # even for a single symbol request - models are agnostic to number of symbols
 # print(bars["GLD"])
 
-assets = [dict(item) for item in bars["GLD"]]
+assets = [dict(item) for item in bars["AAPL"]]
 df = pd.DataFrame.from_records(assets)
-print(df.head())
 
 def historic_data():
-    print('Historic')
+    path = pathlib.Path('D:\Data Science\Real-Time-Data-Analysis\data\\raw\data.csv')
+    data = pd.read_csv(path)
+    final = pd.concat([data, df], ignore_index=True)
+    final.to_csv(path,index=False)
+    print(final.tail(10))
 
 def live_data():
-    print('Live')
+    # print('Live')
+    df = yf.download(tickers = "INFY", start="2022-08-01", end="2022-11-01" ,interval="1h")
+    path = pathlib.Path('D:\Data Science\Real-Time-Data-Analysis\data\\raw\data.csv')
+    data = pd.read_csv(path)
+    df.reset_index(inplace=True)
+    final = pd.concat([data, df], ignore_index=True)
+    final.to_csv(path,index=False)
+    print(final.head())
 
 if __name__ == '__main__':
     
